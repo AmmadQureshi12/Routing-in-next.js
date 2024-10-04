def0fc28522edfaa4ac0df6eee080fae3e0c761c
Ammad Qureshi <159720868+AmmadQureshi12@users.noreply.github.com>
2024-10-04 13:28:19 -0700

    Add files via upload

---
+++ contact.tsx
@@ -0,0 +1,8 @@
+import Header from "@/compunents/Header"
+export default function contact(){
+    return(<div> 
+        <Header />
+        <h1 className="flex gap-10 bg-red-500">Ammadq196@gmail.com</h1>
+        <img src="https://e0.pxfuel.com/wallpapers/997/392/desktop-wallpaper-gmail-3d-logo-gray-brickwall-creative-postal-services-gmail-logo-3d-art-gmail.jpg" alt="s"  />
+    </div>)
+}
