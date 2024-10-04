08e7f65de089b97c76df3b6a2d909cac86345756
Ammad Qureshi <159720868+AmmadQureshi12@users.noreply.github.com>
2024-10-04 13:27:41 -0700

    Add files via upload

---
+++ header.tsx
@@ -0,0 +1,10 @@
+import Header from "@/compunents/Header"
+
+export default function about  () {
+  return (<div>
+    <Header />
+    <h1 className="flex gap-10 bg-orange-400">My Name is Ammad Qureshi</h1>
+    <img src="https://img.freepik.com/premium-photo/hacker-desktop-wallpaper-cyber-background_910608-3043.jpg" alt="s" />
+  </div>)
+}
+ 
